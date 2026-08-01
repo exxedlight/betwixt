c521338e87cee25b2ed11b9a3b9a132f3cae630f
exxedlight <exxedlight@gmail.com>
2026-08-01 03:21:50 +0300

    player restyled

--- widgets/modules/top-bar/bar-player/bar-player.tsx
+++ widgets/modules/top-bar/bar-player/bar-player.tsx
@@ -4,12 +4,14 @@ import {
     trackTitle,
     isPlaying,
     togglePlayPause,
+    trackArtist,
 } from "../../../../lib/services/mpris"
 
 import Cava from "./modules/cava"
 import ProgressBar from "./modules/progress"
 import Pango from "gi://Pango"
 import { tooglePlayerNativeWindow } from "../../../../lib/services/player-toogle"
+import { createComputed } from "gnim"
 
 const META_WIDTH = 250
 // Slightly under META_WIDTH so the ellipsis has a little breathing room
@@ -17,14 +19,18 @@ const META_WIDTH = 250
 const TITLE_WIDTH = 220
 
 export default function BarPlayer() {
+
+    const metaTitle = createComputed(() => 
+        `${trackTitle()}${trackArtist() !== "Unknown Artist" ? ` - ${trackArtist()}` : ""}`
+    )
+
     return (
         <box class="bar-player">
-            <box class={isPlaying.as(p => `play-pause ${p ? "playing" : "stopped"}`)}>
-                <label
-                    xalign={0.5}
-                    label={isPlaying.as(p => p ? "󰏤" : "󰐊")}
-                    $={onClick(() => togglePlayPause())}
-                />
+            <box 
+                class={isPlaying.as(p => `play-pause ${p ? "playing" : "stopped"}`)} 
+                $={onClick(() => togglePlayPause())}
+            >
+                <label label={isPlaying.as(p => p ? "󰏤" : "󰐊")} xalign={0.5} />
             </box>
 
             <box
@@ -35,7 +41,7 @@ export default function BarPlayer() {
                 <ProgressBar barWidth={META_WIDTH} />
                 <box class="meta" widthRequest={META_WIDTH} valign={Gtk.Align.END}>
                     <label
-                        label={trackTitle}
+                        label={metaTitle}
                         class="track-title"
                         ellipsize={Pango.EllipsizeMode.END}
                         maxWidthChars={38}
